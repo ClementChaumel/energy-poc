78a084e970ed68ac42d2880b0fe51f3cf96691d7
Clement Chaumel <clement.chaumel@gmail.com>
2019-09-06 15:34:50 +0100

    :white_check_mark: Update package.json scripts

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,12 @@
 import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-// Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
-
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-
-    // Step 3: Extract the styles as <style> tags
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />)); // eslint-disable-line react/jsx-props-no-spreading
     const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
     return { ...page, styleTags };
   }
 
@@ -28,7 +20,6 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://d3tbpaf5tfzpa.cloudfront.net/buenos-aires.css" />
           <link rel="stylesheet" href="https://d3tbpaf5tfzpa.cloudfront.net/source-sans-pro.css" />
           <link rel="stylesheet" href="https://ddqol34nhllwe.cloudfront.net/reset.css" />
-          {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
         <body>
